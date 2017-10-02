5305ff023ba37a0bc7bbf42a5cb4b7d2d9b70d9b
Lucatorze <lucas.roberts@laposte.net>
2017-10-02 15:43:26 +0200

    add component task

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,28 @@
 {
     'use strict';
 
+    const Task = {
+        props: ["tasks", "task", "i"],
+        template: `
+            <li class="collection-item">
+                <input type="checkbox" :id="'t_' + (i+1)" v-model="task.isDone">
+                <label :for="'t_' + (i+1)" >{{ task.title }}</label>
+                <a href="#" class="link-delete" title="Supprimer cette tâche">
+                    <i class="small material-icons" @click="remove(task)">delete_forever</i>
+                </a>
+            </li>
+        `,
+        methods: {
+            remove: function (task) {
+                this.tasks.splice(this.tasks.indexOf(task), 1)
+            },
+        }
+    };
+
     let vm = new Vue({
         // L'élément définissant le périmètre d'action de l'application Vue.js
         el: 'main#app',
+        components: {"task" : Task},
         data:{
             tasks : [
                 {title : "Nourrir le chat", isDone : false},
@@ -31,9 +50,7 @@
                 this.tasks.push({title: value, isDone: false});
                 this.newTask = ''
             },
-            remove: function (task) {
-                this.tasks.splice(this.tasks.indexOf(task), 1)
-            },
+
         }
     })
 }
